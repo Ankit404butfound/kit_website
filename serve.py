c4cedd676e60946dba7cb16647751307a762a312
Ankit Raj Mahapatra <ankitrajmahapatra@gmail.com>
2022-01-20 15:25:01 +0530

    Fix

--- serve.py
+++ serve.py
@@ -8,4 +8,4 @@ app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 def hello(filename):
     return open('code.html', encoding="utf-8").read().replace("<--CODE_INSERTION_AREA-->", open(f"html/{filename}.html", encoding="utf-8").read())
 
-app.run("0.0.0.0", port=5000)
+app.run("0.0.0.0")
