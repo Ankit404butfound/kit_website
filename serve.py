d73cda3aaf5acc1fde3c95db7c039026f148899c
Ankit Raj Mahapatra <ankitrajmahapatra@gmail.com>
2022-01-20 15:21:46 +0530

    Fix

--- serve.py
+++ serve.py
@@ -4,8 +4,8 @@ app = Flask(__name__, template_folder='', static_url_path="")
 app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 
 
-@app.route('/s')
-def hello():
-    return render_template('index.html')
+@app.route('/<filename>')
+def hello(filename):
+    return open('code.html', encoding="utf-8").read().replace("<--CODE_INSERTION_AREA-->", open(f"html/{filename}.html", encoding="utf-8").read())
 
 app.run("0.0.0.0", port=5000)
