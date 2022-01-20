09acfa30e13378dd989968b1462ae40ecb2b3fd0
Ankit Raj Mahapatra <ankitrajmahapatra@gmail.com>
2022-01-20 16:00:08 +0530

    Fix

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template
 import os
 
-app = Flask(__name__, template_folder='', static_url_path="")
+app = Flask(__name__, template_folder='')
 app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 
 
