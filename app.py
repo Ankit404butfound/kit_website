5e23eca3e5b23aeeddda97b543c04e9a5b8f8726
Ankit Raj Mahapatra <ankitrajmahapatra@gmail.com>
2022-01-20 15:30:56 +0530

    Fix

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+import os
 
 app = Flask(__name__, template_folder='', static_url_path="")
 app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
@@ -8,4 +9,4 @@ app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 def hello(filename):
     return open('code.html', encoding="utf-8").read().replace("<--CODE_INSERTION_AREA-->", open(f"html/{filename}.html", encoding="utf-8").read())
 
-app.run("0.0.0.0")
+app.run("0.0.0.0", port=int(os.environ.get('PORT'))
