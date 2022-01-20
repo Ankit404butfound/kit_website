2d9f3363831287013123c170e9ba413c46c88eed
Ankit Raj Mahapatra <ankitrajmahapatra@gmail.com>
2022-01-20 19:45:18 +0530

    Fix

--- app.py
+++ app.py
@@ -1,12 +1,16 @@
 from flask import Flask, render_template
 import os
 
-app = Flask(__name__, template_folder='')
+app = Flask(__name__)
 app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 
 
 @app.route('/')
 def hello():
-    return render_template('home.html')
+    return render_template("home.html")
+
+@app.route("/<function>")
+def render_function(function):
+    return render_template("code.html", code_to_insert=open(f"templates/{function}.html", encoding="utf-8").read())
 
 app.run("0.0.0.0", port=int(os.environ.get('PORT', 5000)))
