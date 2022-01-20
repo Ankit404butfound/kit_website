ec6516a1d4224cbc2e2b82847bab9c01b8bfbda0
Ankit Raj Mahapatra <ankitrajmahapatra@gmail.com>
2022-01-20 14:41:56 +0530

    Add example

--- example/sendwhatmsg.py
+++ example/sendwhatmsg.py
@@ -1,5 +1,15 @@
+"""
+Version 5.2
+Status: Stable
+Documentation: https://github.com/Ankit404butfound/PyWhatKit/wiki
+Report Bugs and Feature Requests here: https://github.com/Ankit404butfound/PyWhatKit/issues
+For further Information, Join our Discord: https://discord.gg/62Yf5mushu
+"""
+
+
 # Import the package
 import pywhatkit
 
 # Send a message
 pywhatkit.sendwhatmsg("+918098989898", "Hello World", 10, 30)
+phone_no: str, message: str, time_hour: int, time_min: int, wait_time: int = 15, tab_close: bool = False, close_time: int = 3
