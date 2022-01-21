22eaca2c45b68b3277d1131f5c51a854eb3e4d29
Ankit Raj Mahapatra <ankitrajmahapatra@gmail.com>
2022-01-21 14:58:08 +0530

    Update

--- app.py
+++ app.py
@@ -12,9 +12,13 @@ app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 def hello():
     return render_template("home.html")
 
+@app.route('/teams')
+def teams():
+    return render_template("teams.html")
+
 @app.route("/<function>")
 def render_function(function):
     func_html = func_template.render_custom_template(json.load(open(f"static/json/{function}.json", encoding="utf-8")))
-    return render_template("code.html", code_to_insert=func_html, title="PyWhatkit/"+function)#function_information=json.load(open(f"templates/json/{function}.json", encoding="utf-8")))
+    return render_template("code.html", code_to_insert=func_html, title="PyWhatKit/"+function)#function_information=json.load(open(f"templates/json/{function}.json", encoding="utf-8")))
 
 app.run("0.0.0.0", port=int(os.environ.get('PORT', 5000)))
