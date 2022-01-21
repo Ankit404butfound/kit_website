293c00cd603403b83ae54819789a1f49086c2ba0
Ankit Raj Mahapatra <ankitrajmahapatra@gmail.com>
2022-01-21 14:09:15 +0530

    Update

---
+++ example/search.py
@@ -0,0 +1,19 @@
+"""
+Version 5.2
+Status: Stable
+Documentation: https://github.com/Ankit404butfound/PyWhatKit/wiki
+Report Bugs and Feature Requests here: https://github.com/Ankit404butfound/PyWhatKit/issues
+For further Information, Join our Discord: https://discord.gg/62Yf5mushu
+
+This is a Python 3.x script to send Whatsapp Messages.
+This code is written by Ankit404butfound and published on GitHub.
+"""
+
+
+# Import the package
+import pywhatkit
+
+# Passing the desired parameters
+pywhatkit.search("PyWhatKit")
+
+# End of the script
